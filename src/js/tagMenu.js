daedb2b66f28b17b4aafaff2e6d341e39d2025a5
norfish <easumlee@gmail.com>
2013-11-18 20:17:06 +0800

    add the scroll animate fx

--- src/js/tagMenu.js
+++ src/js/tagMenu.js
@@ -1,6 +1,9 @@
 (function(){
 	'use strict';
 
+	var menuScroll = false;
+
+	//私有属性
 	function TagMenu (element, options){
 		this.element = element;
 		this.options = options;
@@ -8,52 +11,34 @@
 		this.init();
 	}
 
-	function pageScroll(step, to) {
-    	window.scrollBy(0,step); // horizontal and vertical scroll increments
-    	//var scrolldelay = setTimeout(pageScroll(),100); // scrolls every 100 milliseconds
-    	var ps = setTimeout(function(){
-    		pageScroll(step);
-		},20);
-		if($(document).scrollTop() > to){
-			clearTimeout(ps);
-		}
+	//使用动画滚动浏览器滚动条
+	function scrollToElement( target ) {
+	    var topoffset = 0;
+	    var speed = 400;
+	    var destination = $( target ).offset().top - topoffset;
+	    $( 'html:not(:animated),body:not(:animated)' ).animate( { scrollTop: destination}, speed, function(){
+	        window.location.hash = $(target).attr('name');
+	        menuScroll = false;
+	    });
+	    return false;
 	}
 
-	function scrollPage(to){
-
-		var dis = to - $(document).scrollTop();
-		var step = dis > 0 ? 20 : -20;
-		var time = Math.ceil(dis/20);
-		var over = time*step - dis;
-		/*while(i<time){
-			setTimeout(function(){
-				window.scrollBy(0,20); 
-			},20);
-			i++;
-		}*/
-
-		//window.scrollBy(0,over);
-		pageScroll(step, to);
-		window.scrollBy(0,over);
-	}
-	
-	function stopScroll() {
-    	clearTimeout(scrolldelay);
-	}
-
+	// 原型属性及方法
 	TagMenu.prototype = {
+
+		// 初始化
 		init: function(){
 			var self = this;
-			var menu = self.element.find('.sideCatalog-item');
-			var tag = $('.tm-tag');
-			self.data.tagpos = [];
-			self.data.menu = menu;
-			$.each(tag, function(k, item){
+			self.data.tag = $('.tm-tag'),
+			self.data.tagpos = [],
+			self.data.menu = self.element.find('.tm-menu');
+
+			$.each(self.data.tag, function(k, item){
 				item = $(item);
 				self.data.tagpos.push(item.offset().top);
 			});
 
-			$.each(menu, function(k, item){
+			$.each(self.data.menu, function(k, item){
 				$(item).data('tag-index', k);
 			})
 			console.log(self.data);
@@ -61,56 +46,60 @@
 
 		// 处理tag内容滚动
 		scroll: function(){
-			//debugger
-			var self = this;
-			var active = self.element.find('.highlight');
-			var index = active.data('tag-index');
-			window.scroll(0, this.data.tagpos[index]);
+			var self = this,
+				active = self.element.find('.highlight'),
+				index = active.data('tag-index'),
+				activetag = self.data.tag[index];
+			setTimeout(function(){
+				menuScroll = true;
+				scrollToElement(activetag);
+			},0);
 			return false;
 		},
 
-		// 处理menu跟随tag滚动而改变
+		// menu跟随tag滚动而改变
 		activeLink: function(){
-			var self = this;
-			var pos = $(document).scrollTop();
-			var index = self.element.find('.highlight').data('tag-index');
-			var tagpos = this.data.tagpos;
-			var menuIndex;
+			var menuIndex,
+				self = this,
+				tagpos = this.data.tagpos,
+				pos = $(document).scrollTop(),
+				index = self.element.find('.highlight').data('tag-index');
 
 			// 预留100像素的边界
 			if(pos < tagpos[index] - 100){
-				menuIndex = index ? index - 1 : 0; 
+				index = index ? index - 1 : 0; 
 			}else if(pos > tagpos[index + 1] - 100){ // index+1 边界
-				menuIndex = index + 1;
+				index = index + 1;
 			}else{
 				return;
 			}
-			var elm = self.element.find('.sideCatalog-item')[menuIndex];
+			var elm = self.element.find('.tm-menu')[index];
 			self.highlight(elm);
 		},
 
 		highlight: function(elm){
-			$(elm).siblings().removeClass('highlight').end().addClass('highlight');
+			elm = $(elm);
+			if(!elm.hasClass('highlight')){
+				elm.siblings().removeClass('highlight').end().addClass('highlight');
+			}
 		}
 	}
-f
+
+	//jquery plugin
 	$.fn.tagmenu = function(){
 		var self = this;
 		var tagmenu = new TagMenu(self,{});
 
-		tagmenu.element.on('click', '.sideCatalog-item', function(){
+		tagmenu.element.on('click', '.tm-menu', function(){
 			tagmenu.highlight(this);
 			tagmenu.scroll();
 		});
 
 		$(document).on('scroll', function(evt){
-			tagmenu.activeLink();
+			if(!menuScroll){
+				tagmenu.activeLink();
+			}
 		})
 	}
-
-	//pageScroll();
-	/*$("html, body").animate({scrollTop:2000}, '1000', 'swing', function() { 
-	   alert("Finished animating");
-	});*/
 
 })(jQuery,window);
